2ccce50cee1171ecdf29cdba950539adfe7b1a6c
oluwakoyajeremiah-art <oluwakoyajeremiah@gmail.com>
2026-08-20 16:45:03 +0000

    added close account logic and updated claim

--- drip_token/programs/drip_token/src/instructions/mod.rs
+++ drip_token/programs/drip_token/src/instructions/mod.rs
@@ -1,11 +1,11 @@
-// src/instructions/mod.rs
 pub mod initialize;
-pub mod claim;
 pub mod update_config;
 pub mod set_vault;
+pub mod claim;
+pub mod close_user_state;
 
-// Re-exporting them for cleaner access in lib.rs
 pub use initialize::*;
-pub use claim::*;
 pub use update_config::*;
 pub use set_vault::*;
+pub use claim::*;
+pub use close_user_state::*;
--- drip_token/programs/drip_token/src/lib.rs
+++ drip_token/programs/drip_token/src/lib.rs
@@ -1,19 +1,20 @@
+use anchor_lang::prelude::*;
+
 pub mod constants;
 pub mod errors;
 pub mod events;
-pub mod instructions; // Declares the instructions folder
+pub mod instructions;
 pub mod state;
 
-use anchor_lang::prelude::*;
 use instructions::*;
 
-declare_id!("2o9Tk9fUu9P7WVEZKpM5aVvFtfpgqbaydNHJLhvHSTxJ");
+declare_id!("9vXiabzy5j6UujwQGb7tPwviA9B1GDBpmKnnsFWu5zSs"); // replace with your real program ID later
 
 #[program]
 pub mod drip_token {
     use super::*;
 
-    pub fn initialize_config(
+    pub fn initialize(
         ctx: Context<InitializeConfig>,
         claim_amount: u64,
         cooldown_seconds: u64,
@@ -21,6 +22,38 @@ pub mod drip_token {
         mint: Pubkey,
         mode: u8,
     ) -> Result<()> {
-        initialize_handler(ctx, claim_amount, cooldown_seconds, daily_limit, mint, mode)
+        instructions::initialize::initialize_handler(ctx, claim_amount, cooldown_seconds, daily_limit, mint, mode)
+    }
+
+    pub fn update_config(
+        ctx: Context<UpdateConfig>,
+        claim_amount: u64,
+        cooldown_seconds: u64,
+        daily_limit: u64,
+        mode: u8,
+        paused: bool,
+        new_admin: Pubkey,
+    ) -> Result<()> {
+        instructions::update_config::update_config_handler(
+            ctx,
+            claim_amount,
+            cooldown_seconds,
+            daily_limit,
+            mode,
+            paused,
+            new_admin,
+        )
+    }
+
+    pub fn set_vault(ctx: Context<SetVault>) -> Result<()> {
+        instructions::set_vault::set_vault_handler(ctx)
+    }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        instructions::claim::claim_handler(ctx)
+    }
+
+    pub fn close_user_state(ctx: Context<CloseUserState>) -> Result<()> {
+        instructions::close_user_state::close_user_state_handler(ctx)
     }
 }
